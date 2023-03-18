af8d19524ccbc9d414f5b123fbff60c78be521e8
seiyoon <sera4084@naver.com>
2023-03-18 14:19:21 +0900

    feat: 자유게시글 작성페이지 UI

--- src/front-end/timepay-front/src/pages/PostPage/PostFreePage.tsx
+++ src/front-end/timepay-front/src/pages/PostPage/PostFreePage.tsx
@@ -1,6 +1,7 @@
-import { Layout, Input } from 'antd';
-import { useNavigate } from 'react-router-dom';
-import { useCallback, useRef, useState, useEffect } from 'react';
+import { Layout, Input, Button } from 'antd';
+import { useNavigate, Link } from 'react-router-dom';
+import { PATH } from '../../utils/paths';
+import { useCallback, useState } from 'react';
 import { ReactComponent as BackArrow } from '../../assets/images/icons/header-back-arrow.svg';
 import { cssMainHeaderStyle } from '../../components/MainHeader/MainHeader.styles';
 import {
@@ -12,76 +13,36 @@ import {
   cssPostFooterStyle,
 } from './PostFreePage.style';
 
-import { CameraFilled } from '@ant-design/icons';
-import { Modal, Upload } from 'antd';
-import type { RcFile, UploadProps } from 'antd/es/upload';
-import type { UploadFile } from 'antd/es/upload/interface';
-
-const getBase64 = (file: RcFile): Promise<string> =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = (error) => reject(error);
-  });
+import ImageUpload from '../../components/Post/ImageUpload';
 
 const { Header, Content, Footer } = Layout;
 const { TextArea } = Input;
 
 const PostFreePage = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
   const navigate = useNavigate();
-  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
-
-  const [value, setValue] = useState<String>();
-
-  useEffect(() => {
-    if (textareaRef && textareaRef.current) {
-      textareaRef.current.style.height = '0px';
-      const scrollHeight = textareaRef.current.scrollHeight;
-      textareaRef.current.style.height = scrollHeight + 'px';
-    }
-  }, [value]);
-
-  /*
-  const scrollRef = useRef<HTMLTextAreaElement>(null);
-
-  useEffect(() => {
-    scrollRef.current?.scrollTo(0, scrollRef.current.scrollHeight);
-  });
-  */
 
   const handleClickBack = useCallback(() => {
     navigate(-1);
   }, [navigate]);
 
-  // 이미지 업로드
-  const [previewOpen, setPreviewOpen] = useState(false);
-  const [previewImage, setPreviewImage] = useState('');
-  const [previewTitle, setPreviewTitle] = useState('');
-  const [fileList, setFileList] = useState<UploadFile[]>([]);
-  const handleCancel = () => setPreviewOpen(false);
+  // 버튼 활성화 관련
+  const isDisabled = !title || !content;
 
-  const handlePreview = async (file: UploadFile) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj as RcFile);
-    }
-
-    setPreviewImage(file.url || (file.preview as string));
-    setPreviewOpen(true);
-    setPreviewTitle(
-      file.name || file.url!.substring(file.url!.lastIndexOf('/') + 1),
-    );
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+  const handleContentChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setContent(event.target.value);
   };
 
-  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) =>
-    setFileList(newFileList);
-
-  const uploadButton = (
-    <div>
-      <CameraFilled />
-      <div style={{ marginTop: 8 }}>이미지 업로드</div>
-    </div>
-  );
+  const handleSubmit = () => {
+    // 게시글 작성 완료 처리
+  };
 
   return (
     <Layout css={cssPostPageStyle}>
@@ -95,6 +56,8 @@ const PostFreePage = () => {
             css={cssPostTitleInputStyle}
             placeholder="제목을 입력하세요"
             maxLength={25}
+            value={title}
+            onChange={handleTitleChange}
           />
           <div css={cssLineStyle} />
           <TextArea
@@ -103,31 +66,23 @@ const PostFreePage = () => {
             style={{ resize: 'none' }}
             css={cssPostContentInputStyle}
             placeholder="내용을 입력하세요"
+            value={content}
+            onChange={handleContentChange}
           />
           <div css={cssLineStyle} />
-          <h5>사진 ({fileList.length}/5)</h5>
-          <Upload
-            className="upload"
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            listType="picture-card"
-            fileList={fileList}
-            onPreview={handlePreview}
-            onChange={handleChange}
-          >
-            {fileList.length >= 5 ? null : uploadButton}
-          </Upload>
-          <Modal
-            open={previewOpen}
-            title={previewTitle}
-            footer={null}
-            onCancel={handleCancel}
-          >
-            <img alt="example" style={{ width: '100%' }} src={previewImage} />
-          </Modal>
+          <ImageUpload />
         </Content>
       </div>
       <Footer css={cssPostFooterStyle}>
-        <button css={cssPostBtnStyle}>작성완료</button>
+        <Link to={PATH.HOME}>
+          <Button
+            css={cssPostBtnStyle}
+            onClick={handleSubmit}
+            disabled={isDisabled}
+          >
+            작성완료
+          </Button>
+        </Link>
       </Footer>
     </Layout>
   );
