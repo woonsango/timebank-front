61abc83d2bbf045c746d91b6dba2a1cac7e428e7
iioo3356 <khy@storelink.io>
2023-03-18 23:25:07 +0900

    feat: 홈페이지 게시글 유형 쿼리스트링 추가 및 헤더 연결

--- src/front-end/timepay-front/src/components/HomeHeader/HomeHeader.tsx
+++ src/front-end/timepay-front/src/components/HomeHeader/HomeHeader.tsx
@@ -1,22 +1,99 @@
 import { BellOutlined, MenuOutlined } from '@ant-design/icons';
 import { Badge, Dropdown, MenuProps } from 'antd';
 import { useMemo } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import {
   cssHomeHeaderMenuStyle,
   cssHomeHeaderNotificationStyle,
   cssHomeHeaderStyle,
 } from './HomeHeader.styles';
+import queryString from 'query-string';
+import { PATH } from '../../utils/paths';
 
 const HomeHeader = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentPostType = useMemo(() => {
+    const qs = queryString.parse(location.search);
+    if (
+      qs &&
+      qs.type &&
+      typeof qs.type === 'string' &&
+      ['전체', '도움요청', '도움제공', '자유', '후기'].includes(qs.type)
+    )
+      return qs.type;
+    else return '전체';
+  }, [location]);
+
   const items: MenuProps['items'] = useMemo(() => {
     return [
-      { label: <div>전체</div>, key: '전체' },
-      { label: <div>도움요청</div>, key: '도움요청' },
-      { label: <div>도움제공</div>, key: '도움제공' },
-      { label: <div>자유</div>, key: '자유' },
-      { label: <div>후기</div>, key: '후기' },
+      {
+        label: (
+          <div
+            onClick={() =>
+              navigate({ pathname: PATH.HOME, search: '?type=전체' })
+            }
+          >
+            전체
+          </div>
+        ),
+        key: '전체',
+        disabled: currentPostType === '전체',
+      },
+      {
+        label: (
+          <div
+            onClick={() =>
+              navigate({ pathname: PATH.HOME, search: '?type=도움요청' })
+            }
+          >
+            도움요청
+          </div>
+        ),
+        key: '도움요청',
+        disabled: currentPostType === '도움요청',
+      },
+      {
+        label: (
+          <div
+            onClick={() =>
+              navigate({ pathname: PATH.HOME, search: '?type=도움제공' })
+            }
+          >
+            도움제공
+          </div>
+        ),
+        key: '도움제공',
+        disabled: currentPostType === '도움제공',
+      },
+      {
+        label: (
+          <div
+            onClick={() =>
+              navigate({ pathname: PATH.HOME, search: '?type=자유' })
+            }
+          >
+            자유
+          </div>
+        ),
+        key: '자유',
+        disabled: currentPostType === '자유',
+      },
+      {
+        label: (
+          <div
+            onClick={() =>
+              navigate({ pathname: PATH.HOME, search: '?type=후기' })
+            }
+          >
+            후기
+          </div>
+        ),
+        key: '후기',
+        disabled: currentPostType === '후기',
+      },
     ];
-  }, []);
+  }, [currentPostType, navigate]);
 
   return (
     <div css={cssHomeHeaderStyle}>
@@ -25,9 +102,11 @@ const HomeHeader = () => {
         menu={{ items }}
         placement="bottomLeft"
         arrow
+        trigger={['click']}
       >
         <div>
-          <MenuOutlined /> 전체
+          <MenuOutlined />
+          {currentPostType}
         </div>
       </Dropdown>
       <Badge dot css={cssHomeHeaderNotificationStyle}>
