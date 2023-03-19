cfce0c141f93947835fa125ec242ae319e8e427c
jin1242 <leejin684@gmail.com>
2023-03-20 02:34:39 +0900

    Edit localhost

--- src/front-end/timepay-front/client/src/pages/LoginPage/kakaoRedirectHandler.tsx
+++ src/front-end/timepay-front/client/src/pages/LoginPage/kakaoRedirectHandler.tsx
@@ -28,7 +28,7 @@ const KakaoRedirectHandler = () => {
   const fetchAuthorizationCode = async () => {
     axios({
       method: 'post',
-      url: 'http://192.168.219.156:5000/authorization',
+      url: 'http://localhost:5000/authorization',
       data: authorizationCode,
     })
       .then((res) => {
