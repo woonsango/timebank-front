f8c68dc3ec64fa4c582849ad2c6534aa258e97b8
Kimmyeonghwan <myeonghwan2@kookmin.ac.kr>
2023-03-20 22:20:56 +0900

    Feat: gson gradle 추가, 카카오 로그인 기능 추가(서버 버그로 인해 테스트X)

--- src/back-end/timepay/src/main/java/com/capstone/timepay/domain/BaseTimeEntity.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/domain/BaseTimeEntity.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@EnableJpaAuditing
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
     @CreatedDate
