a1da07d0a948c90d7cd57e700b8226af9e1e5e84
Kimmyeonghwan <myeonghwan2@kookmin.ac.kr>
2023-03-21 23:02:56 +0900

    Chore: 주석 추가 및 파일명 변경

--- src/back-end/timepay/src/main/java/com/capstone/timepay/controller/user/CreateUserController.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/controller/user/CreateUserController.java
@@ -8,6 +8,7 @@ public class CreateUserController {
 
     /* 회원가입 버튼 클릭하면 데이터가 Post로 json 형식으로 넘어옴 */
     /* json 형식의 데이터를 받아서 UserSignUpService로 넘겨줌 */
+    /* 카카오 데이터와 어떻게 매칭해줄지 생각 필요 */
     @PostMapping("user/create")
     public void createUser(){
 
