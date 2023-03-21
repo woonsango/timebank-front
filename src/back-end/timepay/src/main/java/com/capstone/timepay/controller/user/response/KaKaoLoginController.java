91c707cb6610f58b3faa45c8c947df506b7ba903
Kimmyeonghwan <myeonghwan2@kookmin.ac.kr>
2023-03-21 13:10:11 +0900

    Feat: 카카오에서 인가코드를 받아 유저 데이터 정보 가져오기 구현 완료

--- src/back-end/timepay/src/main/java/com/capstone/timepay/controller/user/response/KaKaoLoginController.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/controller/user/response/KaKaoLoginController.java
@@ -1,9 +1,9 @@
 package com.capstone.timepay.controller.user.response;
 
+import com.capstone.timepay.service.user.KakaoLoginService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 
 @RestController
@@ -11,7 +11,7 @@ public class KaKaoLoginController {
 
     @GetMapping("/login")
     public void kakaoCallback(@RequestParam String code)  {
-        System.out.println(code);
+        String access_Token = KakaoLoginService.getKaKaoAccessToken(code);
+        KakaoLoginService.createKakaoUser(access_Token);
     }
-
 }
--- src/back-end/timepay/src/main/java/com/capstone/timepay/service/user/KakaoLoginService.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/service/user/KakaoLoginService.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Service;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
 @Service
 
 public class KakaoLoginService {
 
-    public String getKaKaoAccessToken(String code){
+    /* velog 대부분 참고 */
+    public static String getKaKaoAccessToken(String code){
         String access_Token="";
         String refresh_Token ="";
         String reqURL = "https://kauth.kakao.com/oauth/token";
@@ -29,8 +31,8 @@ public class KakaoLoginService {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
-            sb.append("&client_id=fadeb1cea8077be1a20d8cc98139a990"); // TODO REST_API_KEY 입력
-            sb.append("&redirect_uri=http://localhost:9000/app/users/kakao"); // TODO 인가코드 받은 redirect_uri 입력
+            sb.append("&client_id=79587b639a3a9ca1c9433fa63bc55863");
+            sb.append("&redirect_uri=http://localhost:8080/login");
             sb.append("&code=" + code);
             bw.write(sb.toString());
             bw.flush();
@@ -65,5 +67,73 @@ public class KakaoLoginService {
         }
 
         return access_Token;
+    }
+
+    public static void createKakaoUser(String token)  { // throws BaseException 오류나와서 보류
+        HashMap<String, Object> userInfo = new HashMap<String, Object>();
+
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+        String email = null;
+        String sex = null;
+        String birthday = null;
+
+        //access_token을 이용하여 사용자 정보 조회
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token전송
+
+            //결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            //Gson 라이브러리로 JSON파싱
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            Long id = element.getAsJsonObject().get("id").getAsLong();
+
+            /* 이메일 제공 여부 확인 및 이메일 가져오기 */
+            boolean hasEmail = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_email").getAsBoolean();
+            if (hasEmail) {
+                email = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
+            }
+
+            /* 성별 제공 여부 확인 및 성별 가져오기 */
+            boolean hasSex = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_gender").getAsBoolean();
+            if (hasSex) {
+                sex = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("gender").getAsString();
+            }
+
+            /* 생일 제공 여부 확인 및 생일 가져오기 */
+            boolean hasBirthday = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_birthday").getAsBoolean();
+            if (hasBirthday) {
+                birthday = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("birthday").getAsString();
+            }
+
+            System.out.println("id : " + id);
+            System.out.println("email : " + email);
+            System.out.println("gender : " + sex);
+            System.out.println("birthday : " + birthday);
+            // userInfo.put(); 데이터베이스에 추가하기 위한 밑작업 도중 커밋 & 푸쉬
+
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
