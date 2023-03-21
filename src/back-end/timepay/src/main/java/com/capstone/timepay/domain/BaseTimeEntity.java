bcadcd506cac8240542be008516307757e9b85e9
JinLee25154 <54920331+JinLee25154@users.noreply.github.com>
2023-03-21 13:08:22 +0900

    Revert "[관리자] #52, #44 회원 조회 / 회원 수정 / 문의 관리 api 개발"

--- src/back-end/timepay/src/main/java/com/capstone/timepay/domain/BaseTimeEntity.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/domain/BaseTimeEntity.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
@@ -12,7 +11,6 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EnableJpaAuditing
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
     @CreatedDate
--- src/back-end/timepay/src/main/java/com/capstone/timepay/domain/user/UserRepository.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/domain/user/UserRepository.java
@@ -2,13 +2,5 @@ package com.capstone.timepay.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<List<User>> findAllByName(String query);
-
-    Optional<List<User>> findAllByNickname(String query);
-
-    Optional<List<User>> findAllByEmail(String query);
 }
