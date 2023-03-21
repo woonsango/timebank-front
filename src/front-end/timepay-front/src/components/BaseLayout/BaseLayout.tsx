7796c0e8cfbbf3d1b72546e1ba9ac5c9a2dd8ff9
seiyoon <sera4084@naver.com>
2023-03-21 23:26:15 +0900

    fix: App.tsx, BaseLayout.tsx 파일 라우터 연결 수정

--- src/front-end/timepay-front/src/components/BaseLayout/BaseLayout.tsx
+++ src/front-end/timepay-front/src/components/BaseLayout/BaseLayout.tsx
@@ -1,13 +1,20 @@
 import { Layout } from 'antd';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { headerTitleState } from '../../states/uiState';
 import MainFooter from '../MainFooter';
 import MainHeader from '../MainHeader';
 import { cssBaseLayoutStyle } from './BaseLayout.styles';
+import { PATH } from '../../utils/paths';
 
 const BaseLayout = () => {
   const headerTitle = useRecoilValue(headerTitleState);
+  const location = useLocation();
+
+  const isFooterVisible =
+    location.pathname === PATH.HOME ||
+    location.pathname === PATH.SEARCH ||
+    location.pathname === PATH.MY_PAGE;
 
   return (
     <Layout css={cssBaseLayoutStyle}>
@@ -19,7 +26,7 @@ const BaseLayout = () => {
       >
         <Outlet />
       </Layout.Content>
-      <MainFooter />
+      {isFooterVisible && <MainFooter />}
     </Layout>
   );
 };
