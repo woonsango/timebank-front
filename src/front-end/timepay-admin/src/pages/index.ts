86f7683ab854e63c9fc44a3c69b79e8289e8b7a9
suyun <suyunup@kookmin.ac.kr>
2023-03-27 05:07:54 +0900

    feat:신고관리화면 추가

--- src/front-end/timepay-admin/src/pages/index.ts
+++ src/front-end/timepay-admin/src/pages/index.ts
@@ -3,6 +3,7 @@ import QnAManagementPage from './QnAManagement';
 import UserManagementPage from './UserManagement';
 import CategoryManagementPage from './CategoryManagement';
 import AdminManagementPage from './AdminManagement';
+import ReportManagementPage from './ReportManagement';
 
 const PageRoutes = {
   UserManagementPage,
@@ -10,6 +11,7 @@ const PageRoutes = {
   PostManagementPage,
   CategoryManagementPage,
   AdminManagementPage,
+  ReportManagementPage,
 };
 
 export default PageRoutes;
