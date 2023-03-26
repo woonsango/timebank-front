b94e00f6caf73962f47328103813dca2299dad9f
suyun <suyunup@kookmin.ac.kr>
2023-03-27 05:08:33 +0900

    feat:신고관리 테이블 추가

---
+++ src/front-end/timepay-admin/src/components/ReportTable/ReportTable.tsx
@@ -0,0 +1,160 @@
+import { Button, Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import { useCallback, useMemo, useState } from 'react';
+import dayjs from 'dayjs';
+import {
+  cssReportTableRowCountStyle,
+  cssReportTableStyle,
+} from './ReportTable.styles';
+import { Item } from '../../interfaces/ReportItem';
+import ReportDetailModal from '../ReportDetailModal';
+
+const ReportTable = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentReport, setCurrentReport] = useState<Item>();
+
+  const handleOnShowDetailReport = useCallback((report: Item) => {
+    setCurrentReport(report);
+    setIsOpen(true);
+  }, []);
+
+  const handleOnCloseDetailReport = useCallback(() => {
+    setCurrentReport(undefined);
+    setIsOpen(false);
+  }, []);
+
+  const dummyDataSource: Item[] = [];
+  for (let i = 0; i < 100; i++) {
+    dummyDataSource.push({
+      reportId: i,
+      name: `홍길동${i + 1}`,
+      number: i + 1,
+      type: i % 2 ? '댓글' : '게시글',
+      content: `신고 사유 ${i + 1}`,
+      postNumber: i,
+      postContent: '신고대상 글내용',
+      reportAt: `2023-12-34 12:34`,
+      authorNumber: i,
+      reportStatus: i % 2 ? 'Y' : 'N',
+    });
+  }
+
+  // @ts-ignore
+  const columns: ColumnsType<Item> = useMemo(() => {
+    return [
+      {
+        title: '신고 번호',
+        key: 'reportId',
+        dataIndex: 'reportId',
+        width: 90,
+        sorter: (a: Item, b: Item) => a.reportId - b.reportId,
+      },
+      {
+        title: '신고자 이름',
+        key: 'name',
+        dataIndex: 'name',
+        width: 100,
+        align: 'center',
+      },
+      {
+        title: '신고자 회원번호',
+        key: 'number',
+        dataIndex: 'number',
+        width: 100,
+        align: 'center',
+      },
+      {
+        title: '신고글 타입',
+        key: 'type',
+        dataIndex: 'type',
+        width: 100,
+        align: 'center',
+        filters: [
+          { text: '댓글', value: '댓글' },
+          { text: '게시글', value: '게시글' },
+        ],
+        onFilter: (value: string, record: Item) =>
+          record.type.indexOf(value) === 0,
+      },
+      {
+        title: '신고 사유',
+        key: 'content',
+        dataIndex: 'content',
+        width: 150,
+        align: 'center',
+        render: (_: string, record: Item) => (
+          <Button type="link" onClick={() => handleOnShowDetailReport(record)}>
+            더보기
+          </Button>
+        ),
+      },
+      {
+        title: '신고대상 글번호',
+        key: 'postNumber',
+        dataIndex: 'postNumber',
+        width: 100,
+        align: 'center',
+      },
+      {
+        title: '신고대상 글내용',
+        key: 'postContent',
+        dataIndex: 'postContent',
+        width: 150,
+        align: 'center',
+        render: (_: string, record: Item) => (
+          <Button type="link" onClick={() => handleOnShowDetailReport(record)}>
+            더보기
+          </Button>
+        ),
+      },
+      {
+        title: '신고일시',
+        key: 'reportAt',
+        dataIndex: 'reportAt',
+        width: 140,
+        align: 'center',
+        sorter: (a: Item, b: Item) =>
+          dayjs(a.reportAt).isAfter(dayjs(b.reportAt)),
+      },
+      {
+        title: '신고대상 회원번호',
+        key: 'authorNumber',
+        dataIndex: 'authorNumber',
+        width: 100,
+        align: 'center',
+      },
+      {
+        title: '신고처리여부',
+        key: 'reportStatus',
+        dataIndex: 'reportStatus',
+        width: 100,
+        align: 'center',
+        filters: [
+          { text: 'Y', value: 'Y' },
+          { text: 'N', value: 'N' },
+        ],
+        onFilter: (value: string, record: Item) =>
+          record.type.indexOf(value) === 0,
+      },
+    ];
+  }, [handleOnShowDetailReport]);
+
+  return (
+    <>
+      <Table
+        css={cssReportTableStyle}
+        columns={columns}
+        scroll={{ x: 1500 }}
+        dataSource={dummyDataSource}
+        rowKey="postId"
+      />
+      <ReportDetailModal
+        isOpen={isOpen}
+        onCancel={handleOnCloseDetailReport}
+        post={currentReport}
+      />
+    </>
+  );
+};
+
+export default ReportTable;
