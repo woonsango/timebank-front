b0e5185a2b98bd1b66e220597e40abcbe8848914
seiyoon <sera4084@naver.com>
2023-03-30 09:33:59 +0900

    fix: InputText.tsx

--- src/front-end/timepay-front/src/components/post/InputText.tsx
+++ src/front-end/timepay-front/src/components/post/InputText.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { cssPostTextarea } from '../../pages/PostPage/PostPage.style';
+import { Input } from 'antd';
 
 interface InputTextProps {
   onChange(e: React.ChangeEvent<HTMLTextAreaElement>): void;
-
   inputText: string;
 }
 
+const { TextArea } = Input;
+
 const InputText = ({ onChange, inputText }: InputTextProps) => {
   return (
-    <>
-      <textarea
-        placeholder="댓글 입력 창 입니다 :)"
-        onChange={(e) => onChange(e)}
-        value={inputText}
-        css={cssPostTextarea}
-        style={{
-          padding: 10,
-          height: 80,
-          width: '90%',
-          fontSize: 20,
-          resize: 'none',
-        }}
-      />
-    </>
+    <TextArea
+      placeholder="댓글 입력 창 입니다 :)"
+      onChange={(e) => onChange(e)}
+      value={inputText}
+      css={cssPostTextarea}
+      style={{
+        padding: 10,
+        height: 80,
+        width: '90%',
+        fontSize: 20,
+        resize: 'none',
+      }}
+    />
   );
 };
 
