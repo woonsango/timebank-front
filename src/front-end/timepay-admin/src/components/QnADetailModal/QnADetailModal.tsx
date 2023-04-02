bb97c69a5448e84cc7d8174ad39dbfe9d1585480
suyun <suyunup@kookmin.ac.kr>
2023-04-02 22:03:54 +0900

    feat:문의 조회 및 답변 작성 모달 추가

---
+++ src/front-end/timepay-admin/src/components/QnADetailModal/QnADetailModal.tsx
@@ -0,0 +1,49 @@
+import { Button, Card, Input, Modal } from 'antd';
+import { useMemo } from 'react';
+import { IQna } from '../../api/interfaces/IQna';
+import { cssQnADetailModalStyle } from './QnADetailModal.styles';
+
+interface QnADetailModalProps {
+  qna?: IQna;
+  isOpen: boolean;
+  onCancel: () => void;
+}
+const { TextArea } = Input;
+const QnADetailModal = ({ qna, isOpen, onCancel }: QnADetailModalProps) => {
+  const footer = useMemo(() => {
+    return (
+      <div>
+        <Button type="primary">답변완료</Button>
+        <Button type="primary" onClick={onCancel}>
+          닫기
+        </Button>
+      </div>
+    );
+  }, [onCancel]);
+
+  return (
+    <Modal
+      title="문의 관리"
+      open={isOpen}
+      onOk={onCancel}
+      onCancel={onCancel}
+      footer={footer}
+      width={700}
+      css={cssQnADetailModalStyle}
+    >
+      <div className="QnATitle">
+        <Card title={qna?.title}>
+          <div className="QnAInnerContent">{qna?.content}</div>
+        </Card>
+        <div className="answerInput">
+          <TextArea
+            className="textInput"
+            placeholder="답변을 입력해주세요"
+            autoSize={{ minRows: 5, maxRows: 5 }}
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+export default QnADetailModal;
