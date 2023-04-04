2acd95c64d10c338575eab8b62977d0272d73d39
iioo3356 <khy@storelink.io>
2023-04-05 02:16:31 +0900

    feat: 관리자 로그인 api 연결

---
+++ src/front-end/timepay-admin/src/api/interfaces/IAdmin.ts
@@ -0,0 +1,12 @@
+export interface IAdmin {
+  createdAt: string;
+  updatedA?: string;
+  adminId: number;
+  adminName: string;
+  password: string;
+  authority: string;
+  name: string;
+  email: string;
+  phone: string;
+  inquiryAnswers: any[];
+}
---
+++ src/front-end/timepay-admin/src/api/urls.ts
@@ -0,0 +1,6 @@
+export const API_URL = {
+  ADMIN_LOGIN: '/api/admins/login',
+};
+
+/** 토큰을 넣지 않아도 되는 api url 모음 */
+export const HEADER_NOT_REQUIRED_URLS = [API_URL.ADMIN_LOGIN];
