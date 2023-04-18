7ec05c4197dd0cb859ec59074c6e43a941210035
Kimmyeonghwan <myeonghwan2@kookmin.ac.kr>
2023-04-18 23:23:27 +0900

    Fix: Board Controller들에서 발생한 import 문제 해결

--- src/back-end/timepay/src/main/java/com/capstone/timepay/controller/board/DealBoardCommentController.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/controller/board/DealBoardCommentController.java
@@ -1,7 +1,6 @@
 package com.capstone.timepay.controller.board;
 
 
-import com.capstone.timepay.controller.board.annotation.Response;
 import com.capstone.timepay.controller.board.request.ReportRequestDTO;
 
 import com.capstone.timepay.domain.dealBoardComment.DealBoardComment;
