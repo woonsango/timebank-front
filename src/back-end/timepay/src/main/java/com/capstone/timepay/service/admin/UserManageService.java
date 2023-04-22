55e6b956285cb24c752d2ddb3bd66b80ad044ef1
wnsdnjs70 <wnsdnjs700@naver.com>
2023-04-22 18:03:05 +0900

    Fix : 관리자 유저 활동 목록 관리 수정

--- src/back-end/timepay/src/main/java/com/capstone/timepay/service/admin/UserManageService.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/service/admin/UserManageService.java
@@ -159,12 +159,10 @@ public class UserManageService {
                 return FreeBoardActivityDto.builder()
                         .boardId(fr.getFreeBoard().getF_boardId())
                         .createdAt(fr.getFreeBoard().getCreatedAt())
-                        .boardType(fr.getFreeBoard().getCategory())
+                        .boardType(fr.getFreeBoard().getType())
                         .boardState(fr.getFreeBoard().getCategory())    // 일단 카테고리로 넣어놓음
                         .startTime(fr.getFreeBoard().getCreatedAt())
                         .endTime(fr.getFreeBoard().getCreatedAt())
-                        .actualStartTime(fr.getFreeBoard().getCreatedAt())
-                        .actualEndTime(fr.getFreeBoard().getCreatedAt())
                         .title(fr.getFreeBoard().getTitle())
                         .build();
             }
@@ -179,12 +177,10 @@ public class UserManageService {
                 return DealBoardActivityDto.builder()
                         .boardId(dr.getDealBoard().getD_boardId())
                         .createdAt(dr.getDealBoard().getCreatedAt())
-                        .boardType(dr.getDealBoard().getCategory())
+                        .boardType(dr.getDealBoard().getType())
                         .boardState(dr.getDealBoard().getBoardStatus())
                         .startTime(dr.getDealBoard().getStartTime())
                         .endTime(dr.getDealBoard().getEndTime())
-                        .actualStartTime(dr.getDealBoard().getStartTime()) // 일단 원래 시작 시간으로 넣어놓음.
-                        .actualEndTime(dr.getDealBoard().getEndTime()) // 일단 원래 시작 시간으로 넣어놓음.
                         .title(dr.getDealBoard().getTitle())
                         .build();
             }
