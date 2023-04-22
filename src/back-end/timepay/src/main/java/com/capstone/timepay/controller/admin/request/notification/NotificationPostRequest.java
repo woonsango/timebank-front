fdf595261c497918b16282f6179c449f9330edca
jun <59019322+mrgentle1@users.noreply.github.com>
2023-04-22 16:08:43 +0900

    feat: 공지사항에 push알림 전송 기능 추가

--- src/back-end/timepay/src/main/java/com/capstone/timepay/controller/admin/request/notification/NotificationPostRequest.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/controller/admin/request/notification/NotificationPostRequest.java
@@ -1,17 +1,20 @@
 package com.capstone.timepay.controller.admin.request.notification;
 
 import com.capstone.timepay.service.admin.dto.NotificationPostDTO;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.modelmapper.ModelMapper;
 
 @ToString
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class NotificationPostRequest {
 
     private String title;
+    @ApiModelProperty(hidden = true)
     private String imageUrl;
     private Boolean isNotice;
     private String content;
