ac1a932d82d3fd3488b0cec40a1d94601b218119
iioo3356 <khy@storelink.io>
2023-04-23 02:46:43 +0900

    feat: 전체 알림 화면에 공지 조회 api 연결

--- src/front-end/timepay-front/src/api/interfaces/IPush.ts
+++ src/front-end/timepay-front/src/api/interfaces/IPush.ts
@@ -1,3 +1,5 @@
+import { PageableData } from './ICommon';
+
 export const PUSH_TYPE = {
   ALL: '전체알림',
   ACTIVITY: '활동관련',
@@ -13,3 +15,22 @@ export interface IPush {
 }
 
 export type IPushType = 'notice' | 'activity' | 'comment' | 'bookmark';
+
+export interface INotification {
+  createdAt: string;
+  updatedAt: string;
+  pushId: null;
+  type: string;
+  isAlreadyRead?: boolean;
+  notificationId: number;
+  title: string;
+  imageUrl: any;
+  content: string;
+  adminId: number;
+  adminName: string;
+  notice: boolean;
+  link: null;
+}
+export interface IGetNotificationResponse extends PageableData {
+  content: INotification[];
+}
--- src/front-end/timepay-front/src/api/urls.ts
+++ src/front-end/timepay-front/src/api/urls.ts
@@ -1,5 +1,6 @@
 export const API_URL = {
   DEAL_BOARDS: '/api/deal-boards',
+  NOTIFICATIONS: 'api/notifications',
 };
 
 export const HEADER_NOT_REQUIRED_URLS = [API_URL.DEAL_BOARDS];
