795401df1b7c206e3759babcd7fa26b7068e7f6d
seiyoon <sera4084@naver.com>
2023-04-23 21:47:00 +0900

    feat: api 설정을 위한 세팅

--- src/front-end/timepay-front/src/api/urls.ts
+++ src/front-end/timepay-front/src/api/urls.ts
@@ -1,7 +1,12 @@
 export const API_URL = {
   DEAL_BOARDS: '/api/deal-boards',
+  FREE_BOARDS: '/api/free-boards',
+  FREE_BOARDS_WRITE: '/api/free-boards/write',
+  INQUIRY_WRITE: '/api/inquiry-boards/write',
+  INQUIRY: 'api/inquiry-boards',
   NOTIFICATIONS: 'api/notifications',
   USERS_INFO: '/api/users/get/',
 };
 
+// 토큰 필요 없는 애들
 export const HEADER_NOT_REQUIRED_URLS = [API_URL.DEAL_BOARDS];
