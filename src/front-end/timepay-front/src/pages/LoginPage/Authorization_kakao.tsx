a7b239455f1d34ee2ab24801e7336a18b9354c3d
jin1242 <leejin684@gmail.com>
2023-04-26 16:34:44 +0900

    save

--- src/front-end/timepay-front/src/pages/LoginPage/Authorization_kakao.tsx
+++ src/front-end/timepay-front/src/pages/LoginPage/Authorization_kakao.tsx
@@ -1,7 +1,7 @@
 //카카오 로그인 페이지
 
 const REST_API_KEY = '79587b639a3a9ca1c9433fa63bc55863'; //카카오-타임페이 커뮤니티 REST API 앱 키
-const REDIRECT_URI = 'http://localhost:3000/oauth/redirect/kakao'; //카카오-타임페이 커뮤니티 Redirect URI, 로컬 IP 주소 넣기
+const REDIRECT_URI = 'http://13.125.119.30:8080/oauth/redirect/kakao'; //카카오-타임페이 커뮤니티 Redirect URI, 로컬 IP 주소 넣기
 //http://13.125.119.30/oauth/redirect/kakao
 //http://localhost:3000/oauth/redirect/kakao
 
