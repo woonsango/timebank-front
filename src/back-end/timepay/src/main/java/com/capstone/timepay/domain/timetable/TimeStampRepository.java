b503df3c4dd9df6692ca7ebe7deab620260c5909
yonghlee <cjswl1357@gmail.com>
2023-04-30 16:33:52 +0900

    feat: 타임테이블 엔티티 추가

---
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/domain/timetable/TimeStampRepository.java
@@ -0,0 +1,6 @@
+package com.capstone.timepay.domain.timetable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeStampRepository extends JpaRepository<TimeStamp, Long> {
+}
---
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/service/timetable/TimeTableService.java
@@ -0,0 +1,9 @@
+package com.capstone.timepay.service.timetable;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TimeTableService {
+}
