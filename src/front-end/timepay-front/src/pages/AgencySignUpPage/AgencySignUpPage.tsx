2a62b3cf731db5f4dfdda4d6bafd7b125c7c5b07
iioo3356 <khy@storelink.io>
2023-05-02 01:29:48 +0900

    chore: 필요없는 콘솔 삭제

--- src/front-end/timepay-front/src/pages/AgencySignUpPage/AgencySignUpPage.tsx
+++ src/front-end/timepay-front/src/pages/AgencySignUpPage/AgencySignUpPage.tsx
@@ -32,7 +32,6 @@ const AgencySignUpPage = () => {
   const [messageApi, contextHolder] = message.useMessage();
 
   const normFile = (e: any) => {
-    console.log('Upload event:', e);
     if (Array.isArray(e)) {
       return e;
     }
