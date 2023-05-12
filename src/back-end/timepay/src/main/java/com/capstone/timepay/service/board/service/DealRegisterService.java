d6b65c88302f55e4612de1e233e2fc043635d2e5
Kimmyeonghwan <myeonghwan2@kookmin.ac.kr>
2023-05-12 18:06:58 +0900

    Fix: (타)유저 프로필 조회 댓글 조회 수정, 자동 매칭 개발 완료

--- src/back-end/timepay/src/main/java/com/capstone/timepay/service/board/service/DealRegisterService.java
+++ src/back-end/timepay/src/main/java/com/capstone/timepay/service/board/service/DealRegisterService.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DealRegisterService {
 
-    private DealRegisterRepository dealRegisterRepository;
+    private final DealRegisterRepository dealRegisterRepository;
 
     public DealRegister getId(Long id)
     {
@@ -23,6 +23,7 @@ public class DealRegisterService {
     // 해당 레지스터에서 유저 Email을 가져오는 메서드
     public String getEmail(Long boardId)
     {
+        System.out.println(boardId);
         DealRegister dealRegister = dealRegisterRepository.findById(boardId).orElse(null);
         return dealRegister.getUser().getEmail();
     }
