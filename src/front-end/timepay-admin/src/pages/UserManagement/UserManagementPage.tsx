83291646cfb72eb184a6d81f6b1e3024544c28c6
jin1242 <leejin684@gmail.com>
2023-05-16 14:17:40 +0900

    관리자 회원관리 error 수정

--- src/front-end/timepay-admin/src/pages/UserManagement/UserManagementPage.tsx
+++ src/front-end/timepay-admin/src/pages/UserManagement/UserManagementPage.tsx
@@ -3,13 +3,11 @@ import { Card, Space, Table } from 'antd';
 import { topWrapperCSS, rightAlignCSS } from './UserManagement.styles';
 import SearchForm from './searchForm';
 
-
 import React from 'react';
 import { useState } from 'react';
 import type { ColumnsType } from 'antd/es/table';
 
 import ProfileImageModal from './profileImageModal';
-import DetailModal from './detailModal';
 import EditModal from './editModal';
 import { IUserInfo } from '../../api/interfaces/IUser';
 
@@ -20,7 +18,6 @@ const UserManagementPage = () => {
   const [selectedUserInfos, setSelectedUserInfos] = useState<IUserInfo[]>();
 
   return (
-
     <Card title="회원 관리" css={topWrapperCSS}>
       <SearchForm />
       <MainTable
