d7c58fe05c6036afb8081116eacbef518a020642
iioo3356 <khy@storelink.io>
2023-05-20 21:38:33 +0900

    chore:await 추가

--- src/front-end/timepay-front/src/utils/device.ts
+++ src/front-end/timepay-front/src/utils/device.ts
@@ -3,7 +3,7 @@ export const isMobileWidth = (windowWidth: number) => windowWidth <= 600;
 export const getDeviceToken = async () => {
   let deviceToken;
   if (window && window.flutter_inappwebview)
-    deviceToken = window.flutter_inappwebview?.callHandler(
+    deviceToken = await window.flutter_inappwebview?.callHandler(
       'getDeviceToken',
       '',
     );
